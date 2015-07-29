cfc7db482de0b440d211669b2fb60ea49dbcad3f
Michael <rubiotran@gmail.com>
2015-07-28 20:52:26 -0700

    Added jQuery masonry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Rails.application.configure do
 # Note to set this to the actual host
 config.action_mailer.default_url_options = { host: 'michaelslivesite.herokuapp.com', port: 3000 }
 
+# Sets Paperclip to upload images to Amazon s3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
